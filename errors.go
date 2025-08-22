14ade6415128029d5841a74416d121475e98b2a4
Go <22589241+go101@users.noreply.github.com>
2025-08-22 18:26:43 +0800

    Add TrackError, for https://github.com/golang/go/issues/74488. TrackError(anIncomparbleErr, anIncomparbleErr) panics.
    Add TrackErrorOf, for https://github.com/golang/go/issues/51945.

--- errors.go
+++ errors.go
@@ -1,6 +1,7 @@
 package nstd
 
 import (
+	"errors"
 	"unique"
 )
 
@@ -19,4 +20,57 @@ type errorString struct {
 
 func (e errorString) Error() string {
 	return e.s.Value()
+}
+
+// TrackError reports whether any error in err's tree matches target.
+// It behaves almost the same as [errors.Is], except that
+// TrackError(anIncomparbleErr, anIncomparbleErr) panics.
+//
+// See: https://github.com/golang/go/issues/74488
+func TrackError(err, target error) bool {
+	if err == nil || target == nil {
+		return err == target
+	}
+
+	return is(err, target)
+}
+
+func is(err, target error) bool {
+	for {
+		if x, ok := err.(interface{ Is(error) bool }); ok && x.Is(target) {
+			return true
+		}
+		if err == target {
+			return true
+		}
+
+		switch x := err.(type) {
+		case interface{ Unwrap() error }:
+			err = x.Unwrap()
+			if err == nil {
+				return false
+			}
+		case interface{ Unwrap() []error }:
+			for _, err := range x.Unwrap() {
+				if is(err, target) {
+					return true
+				}
+			}
+			return false
+		default:
+			return false
+		}
+	}
+}
+
+// TrackErrorOf finds the first error in err's tree that matches ErrorType,
+// and if one is found, returns a pointer to a copy of that error.
+// Otherwise, it returns nil.
+func TrackErrorOf[ErrorType error](err error, _ ...ErrorType) *ErrorType {
+	var e ErrorType
+	if errors.As(err, &e) {
+		return &e
+	}
+
+	return nil
 }
--- errors_test.go
+++ errors_test.go
@@ -1,6 +1,7 @@
 package nstd
 
 import (
+	"errors"
 	"strings"
 	"testing"
 )
@@ -18,5 +19,93 @@ func TestNewError(t *testing.T) {
 	}
 	if e3 != e2 {
 		t.Fatal("NewError: e3 != e2")
+	}
+}
+
+type IncomparableError struct {
+	x []int
+	error
+}
+
+func (ie *IncomparableError) Unwrap() error {
+	return ie.error
+}
+
+type ComparableError struct {
+	x [8]int
+	error
+}
+
+func TestTrackError(t *testing.T) {
+	var ie = IncomparableError{
+		x:     []int{1, 2},
+		error: errors.New("test"),
+	}
+	func() {
+		defer func() {
+			if v := recover(); v == nil {
+				t.Fatal("TrackError(ie, ie) should panic.")
+			}
+		}()
+
+		TrackError(ie, ie)
+		panic("unreachable")
+	}()
+
+	func() {
+		defer func() {
+			if v := recover(); v == nil {
+				t.Fatal("TrackError(ce, ce) should panic.")
+			}
+		}()
+
+		var ce = ComparableError{error: ie}
+		TrackError(ce, ce)
+		panic("unreachable")
+	}()
+
+	var pie = &ie
+	if !TrackError(pie, pie) {
+		t.Fatal("TrackError(pie, pie) should return true.")
+	}
+	var ce = ComparableError{error: pie}
+	if !TrackError(ce, ce) {
+		t.Fatal("TrackError(ce, ce) should return true.")
+	}
+}
+
+func TestTrackErrorOf(t *testing.T) {
+	var ce = ComparableError{}
+
+	var ie = IncomparableError{
+		error: ce,
+	}
+
+	if TrackErrorOf[ComparableError](ie) != nil {
+		t.Fatal("TrackErrorOf[ComparableError](ie) != nil")
+	}
+	if TrackErrorOf(ie, ce) != nil {
+		t.Fatal("TrackErrorOf(ie, ce) != nil")
+	}
+
+	if TrackErrorOf[ComparableError](&ie) == nil {
+		t.Fatal("TrackErrorOf[ComparableError](&ie) == nil")
+	}
+	if TrackErrorOf(&ie, ce) == nil {
+		t.Fatal("TrackErrorOf(&ie, ce) == nil")
+	}
+
+	if TrackErrorOf[IncomparableError](ce) != nil {
+		t.Fatal("TrackErrorOf[IncomparableError](ce) != nil")
+	}
+	if TrackErrorOf(ce, ie) != nil {
+		t.Fatal("TrackErrorOf(ce, ie) != nil")
+	}
+
+	if TrackErrorOf[IncomparableError](&ce) != nil {
+		t.Fatal("TrackErrorOf[IncomparableError](ce) != nil")
+	}
+	if TrackErrorOf(&ce, ie) != nil {
+		t.Fatal("TrackErrorOf(&ce, ie) != nil")
 	}
 }
